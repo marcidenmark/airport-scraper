f434b70d9b085e5235b29a3d75d97eda8cc40225
marci papineau <marcidenmark@hotmail.com>
2018-03-16 09:21:45 +0100

    changed ruby version, added gem file and got some ruby scraping code working

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+# Added at 2018-03-15 17:58:17 +0100 by marcidenmark:
+gem "nokogiri", "~> 1.8"
+
+# Added at 2018-03-15 18:02:29 +0100 by marcidenmark:
+gem "pry", "~> 0.11.3"
--- practice.rb
+++ practice.rb
@@ -1,12 +1,33 @@
 require "open-uri"
 require "nokogiri"
+require 'pry'
 
 # Information required
 # - City names
 # - Arrival times
 # - Destination times
 # - Flight numbers
+#
+# struct is a quick way to build a class with named accessors.
+#
+#
+class Flight
+  attr_accessor :arrival_city, :arrival_time, :destination_city, :destination_time, :flight_number
+  def initialize(arrival_time: nil, arrival_city: nil, destination_city: nil, destination_time: nil, flight_number: nil)
+	 @arrival_time = arrival_time
+	 @arrival_city = arrival_city
+	 @destination_city = destination_city
+	 @destination_time = destination_time
+	 @flight_number = flight_number
+  end
 
+  def to_h
+    {
+		arrival_time: arrival_time
+    }
+  end
+end
+# Flight = Struct.new(:arrival_city, :arrival_time, :destination_city, :destination_time, :flight_number)
 noko_doc = Nokogiri::HTML(open("https://www.cph.dk/flyinformation/ankomster/"))
 
 # wrong_selector = '.flights__table__col--destination span'
@@ -15,7 +36,16 @@ noko_doc = Nokogiri::HTML(open("https://www.cph.dk/flyinformation/ankomster/"))
 correct_selector = '.flights__table .stylish-table__row--body'
 flights_divs = noko_doc.search(correct_selector)
 
+flights = []
 flights_divs.each_with_index do |div, index|
+	# binding.pry
+	flight = Flight.new
+
+	tid = div.css('.stylish-table__cell.flights__table__col--time').to_a.first.text.strip
+
+	flight.arrival_time = tid
+	flights << flight
+
     # puts "#{index + 2}. #{element.text.strip}"
     # Alright, everything are nokogiri elements
     # On noko elements, you can also call the search method again:
@@ -24,7 +54,7 @@ flights_divs.each_with_index do |div, index|
     arrival_time = div.search(arrival_time_selector).text.strip
     # Looks like there is some cleanup to do though
     # I'll let you do that :)
-    p arrival_time
+    # p arrival_time
 
     # Now what's next?
     # Well, you can look for the rest of the data. And remember, now you are iterating
@@ -35,115 +65,9 @@ flights_divs.each_with_index do |div, index|
 end
 
 
-arivals = Nokogiri::HTML(open("https://www.cph.dk/flyinformation/ankomster/"))
-
-arivals.search('.flights__table__col--destination span').each_with_index do |element, index|
-	puts "#{index + 2}. #{element.text.strip}"
-end
-
-# arivals.search('.flights__table__col--destination')[1].text.strip.match(/[^\t\r\n]*/).each_with_index do |element, index|
-# 	puts "#{index + 2}. #{element.text.strip}"
+# flights.each { |flight| puts flight.to_h }
+require 'json'
+puts JSON.generate(flights.map { |flight| flight.to_h })
+# flights.each do |flight|
+#   puts flight.to_h
 # end
-
-
-# doc = Nokogiri::HTML(open("https://www.cph.dk/flyinformation/ankomster/"))
-# nodeset = doc.search('.flights__table__col--destination span')
-
-
-# nodeset.map.with_index{ |x, i| x * i }
-# puts nodeset.size
-
-# puts nodeset[0]
-
-# puts nodeset[1..20].text.uniq
-
-# puts [1,2,3].each {|index| puts nodeset[index].text.strip }
-# def nodeset_toarray ()
-
-# return nodeset_toarray
-# end
-
-# WITH SCAPPING ADVICE
-puts "You can do it MARCI!"
-
-# Nokogiri::HTML(open(url).read).search('.flights__table__col--destination')[1].text.strip.match(/[^\t\r\n]*/)
-
-
-
-p [1, 2, 3, 4, 5].select { |number| number.odd? }
-
-doc = Nokogiri::HTML(open("https://www.cph.dk/flyinformation/ankomster/"))
-nodeset = doc.search('.flights__table__col--destination span')
-
-nodeset.size
-# 1456
-nodeset[0]
- #(Element:0x3ffe74c92900 { name = "span", children = [ #(Text "\r\n\t\t\t\t\t\t\tFra\r\n\t\t\t\t\t\t")] })
-nodeset[0].text
-# "\r\n\t\t\t\t\t\t\tFra\r\n\t\t\t\t\t\t"
-nodeset[0].text.strip
-#  "Fra"
-[1,2,3].each {|index| puts nodeset[index].text.strip };0
-# Agadir/Marocco
-# Agadir/Marocco
-# Norwegian Air International
-
-values=["4" ,"4", "2"]
-p values.uniq
-# ______________________________________________
-
-arivals = Nokogiri::HTML(open("https://www.cph.dk/flyinformation/ankomster/"))
-
-arivals.search('.flights__table__col--destination span').each_with_index do |element, index|
-	puts "#{index + 2}. #{element.text.strip}"
-end
-
-# arivals.search('.flights__table__col--destination')[1].text.strip.match(/[^\t\r\n]*/).each_with_index do |element, index|
-# 	puts "#{index + 2}. #{element.text.strip}"
-# end
-
-
-# doc = Nokogiri::HTML(open("https://www.cph.dk/flyinformation/ankomster/"))
-# nodeset = doc.search('.flights__table__col--destination span')
-
-
-# nodeset.map.with_index{ |x, i| x * i }
-# puts nodeset.size
-
-# puts nodeset[0]
-
-# puts nodeset[1..20].text.uniq
-
-# puts [1,2,3].each {|index| puts nodeset[index].text.strip }
-# def nodeset_toarray ()
-
-# return nodeset_toarray
-# end
-
-
-# Nokogiri::HTML(open(url).read).search('.flights__table__col--destination')[1].text.strip.match(/[^\t\r\n]*/)
-
-
-
-
-
-
-
-
-
-
-
-
-# class="stylish-table__cell flights__table__col--time"
-# 		<span>
-# 								00:15
-# 							</span>
-# 		<div class="stylish-table__cell v--desktop-only">
-# 		<div>
-# 			<span>
-# 				<span>
-# 					Norwegian Air International
-# 				</span>
-# 						</span>
-# 					</div>
-# 				</div>
